0ce9d91d3563fe901c5f7228f36265f5f73a5c41
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-06-05 19:19:12 -0500

    tests carried out in todoReducer

--- src/tests/components/todoReducer.test.js
+++ src/tests/components/todoReducer.test.js
@@ -11,7 +11,7 @@ describe('Pruebas en todoReducer', () => {
     });
 
     test('Debe de agregar un nuevo todo', () => {
-        
+
         const newTodo = {
           id: 3,
           text: 'Nuevo Todo',
@@ -27,4 +27,29 @@ describe('Pruebas en todoReducer', () => {
         expect( state ).toEqual([...demoTodos, newTodo]);
     });
 
+    test('Debe de eliminar un todo', () => {
+
+      const action = {
+        type: 'delete',
+        payload: 1
+      };
+
+      const state = todoReducer( demoTodos, action );
+      /* Checking that the state is equal to the demoTodos array with the todo with id 1 removed. */
+      expect( state ).toEqual([...demoTodos.filter(todo => todo.id !== 1)]);
+    })
+
+    test('Debe de modificar el estado de un todo', () => {
+
+      /* Toggling the done property of the todo with id 1. */
+      const action = {
+        type: 'toggle',
+        payload: 1
+      };
+
+      const state = todoReducer( demoTodos, action );
+    /* Checking that the state is equal to the demoTodos array with the todo with id 1 having
+      its done property toggled. */
+      expect( state ).toEqual([...demoTodos.map(todo => todo.id === 1 ? {...todo, done: !todo.done} : todo)]);
+    });
 })
